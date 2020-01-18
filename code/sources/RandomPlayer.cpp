be643024d9a6f89dec560dedb83946134461db96
Niels <n.a.de.jong@student.rug.nl>
2020-01-18 11:09:27 +0100

    Implement timing for random players (trivially)

--- code/sources/RandomPlayer.cpp
+++ code/sources/RandomPlayer.cpp
@@ -4,4 +4,8 @@ RandomPlayer::RandomPlayer(Maze *m) : Player(m, 0.0, true) {}
 
 RandomPlayer::~RandomPlayer() = default;
 
-void RandomPlayer::solveMaze() {}
+void RandomPlayer::solveMaze() {
+    auto startTime = std::chrono::high_resolution_clock::now();
+    auto endTime = std::chrono::high_resolution_clock::now();
+    this->epochTimings.push_back(std::chrono::duration_cast<std::chrono::nanoseconds>(endTime - startTime).count() / 1e3);
+}
