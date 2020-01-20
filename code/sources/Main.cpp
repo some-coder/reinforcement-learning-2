574813405becf702e6031678df9e50b48ab42f43
Niels <n.a.de.jong@student.rug.nl>
2020-01-20 22:30:11 +0100

    Finally document all code

--- code/sources/Main.cpp
+++ code/sources/Main.cpp
@@ -2,6 +2,13 @@
 #include "Maze.hpp"
 #include "UserInput.hpp"
 
+/**
+ * Executes the program.
+ *
+ * @param argc The number (count) of command-line arguments.
+ * @param argv A vector of command-line arguments.
+ * @return The exit signal.
+ */
 int main(int argc, char *argv[]) {
     RandomServices::initialiseRandomServices();
     Experiment e = UserInput::experimentFromUserInput();
